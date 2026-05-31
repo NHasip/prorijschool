b1ac8a210a30b17fcd3e1f907d3622d6e6d40c72
NHasip <neciphasip@gmail.com>
2026-05-31 18:54:26 +0200

    feat: render role dashboards from stitch exports

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,21 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\View\View;
+use Symfony\Component\HttpFoundation\Response;
 
 class DashboardController extends Controller
 {
-    public function index(Request $request): View
+    public function index(Request $request): Response
     {
         /** @var User $user */
         $user = $request->user();
 
-        return view('dashboard.index', [
-            'user' => $user,
-            'pendingLeerlingenCount' => User::query()
-                ->where('role', User::ROLE_LEERLING)
-                ->where('approval_status', 'pending')
-                ->count(),
-        ]);
+        $screenId = match ($user->role) {
+            User::ROLE_ADMIN, User::ROLE_BEHEERDER => '49e1b4a9d8d047dd93a57cb7ad15b837',
+            User::ROLE_INSTRUCTEUR => '22f07a112aba424084edd5dca9c54fe6',
+            default => '0413e936266e41c493dcc562d3032b00',
+        };
+
+        return app(StitchDesignController::class)->show($screenId);
     }
 }
